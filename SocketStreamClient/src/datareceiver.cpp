5ff59de47475a74a2dc254577cf9aeeb9fec8876
spectralcode <51879191+spectralcode@users.noreply.github.com>
2024-02-25 18:12:36 +0100

    moved to src directory

--- SocketStreamClient/src/datareceiver.cpp
+++ SocketStreamClient/src/datareceiver.cpp
@@ -1,85 +1,82 @@
-/**
-**  This file is part of Socket Stream Client.
-**  Socket Stream Client can be used to test Socket Stream Extension for OCTproZ
-**  Copyright (C) 2020 Miroslav Zabic
-**
-**  Socket Stream Client is free software: you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, either version 3 of the License, or
-**  (at your option) any later version.
-**
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program. If not, see http://www.gnu.org/licenses/.
-**
-****
-** Author:	Miroslav Zabic
-** Contact:	zabic
-**			at
-**			iqo.uni-hannover.de
-****
-**/
+///**
+//**  This file is part of Socket Stream Client.
+//**  Socket Stream Client can be used to test Socket Stream Extension for OCTproZ
+//**  Copyright (C) 2020,2024 Miroslav Zabic
+//**
+//**  Socket Stream Client is free software: you can redistribute it and/or modify
+//**  it under the terms of the GNU General Public License as published by
+//**  the Free Software Foundation, either version 3 of the License, or
+//**  (at your option) any later version.
+//**
+//**  This program is distributed in the hope that it will be useful,
+//**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//**  GNU General Public License for more details.
+//**
+//**  You should have received a copy of the GNU General Public License
+//**  along with this program. If not, see http://www.gnu.org/licenses/.
+//**
+//****
+//** Author:	Miroslav Zabic
+//** Contact:	zabic
+//**			at
+//**			spectralcode.de
+//****
+//**/
 
 #include "datareceiver.h"
-#include <QApplication>
+#include <QtMath>
 
-DataReceiver::DataReceiver(QObject *parent) : QObject(parent)
-{
-	this->socket = new QTcpSocket(this);
-	connect(this->socket, &QTcpSocket::readyRead, this, &DataReceiver::readIncommingData);
-	connect(this->socket, &QTcpSocket::connected, [this]() {
-		emit connected(true);
-	});
-	connect(this->socket, &QTcpSocket::disconnected, [this]() {
-		emit connected(false);
-	});
+DataReceiver::DataReceiver(QObject *parent) : QObject(parent), socket(new QTcpSocket(this)) {
+	connect(socket, &QTcpSocket::readyRead, this, &DataReceiver::readIncomingData);
+	connect(socket, &QTcpSocket::connected, this, [this]() { emit this->connected(true); });
+	connect(socket, &QTcpSocket::disconnected, this, [this]() { emit this->connected(false); });
 
-	this->params.bitDepth = 0;
-	this->params.samplesPerLine = 0;
-	this->params.linesPerFrame = 0;
-	this->params.framesPerBuffer = 0;
-	this->bufferSize = 0;
-	this->bytesWritten = 0;
-	for(int i = 0; i < BUFFERS; i++){
-		this->buff[i] = (char*)malloc(128);
+	buffers.resize(BUFFERS);
+	for (int i = 0; i < BUFFERS; ++i) {
+		buffers[i].resize(128); // Initial size, adjust as needed
 	}
-	this->currentIndex = 0;
 }
 
 DataReceiver::~DataReceiver() {
-	for(int i = 0; i < BUFFERS; i++){
-		free(this->buff[i]);
-	}
+	//no need for manual cleanup due to smart pointers and Qt parent-child mechanism
 }
 
-void DataReceiver::readIncommingData() {
-	while(this->socket->bytesAvailable()){
-		size_t incommingBytes = this->socket->bytesAvailable();
-		memcpy(&(this->buff[currentIndex][bytesWritten]), this->socket->readAll().data(), incommingBytes);
-		this->bytesWritten += incommingBytes;
-		if(this->bytesWritten >= this->bufferSize-1){
-			emit dataAvailable(this->buff[this->currentIndex], this->params.bitDepth, this->params.samplesPerLine, this->params.linesPerFrame);
+void DataReceiver::readIncomingData() {
+	while (this->socket->bytesAvailable()) {
+		QByteArray incomingData = this->socket->readAll();
+		int spaceLeftInBuffer = this->bufferSize - this->bytesWritten;
+		int bytesToWrite = qMin(incomingData.size(), spaceLeftInBuffer);
+
+		//ensure the buffer is large enough to hold the incoming data
+		if(bytesToWrite > this->buffers[this->currentIndex].size() - this->bytesWritten){
+			this->buffers[this->currentIndex].resize(this->bytesWritten + bytesToWrite);
+		}
+
+		//copy the incoming data into the current buffer
+		std::copy(incomingData.begin(), incomingData.begin() + bytesToWrite, this->buffers[currentIndex].begin() + bytesWritten);
+		this->bytesWritten += bytesToWrite;
+
+		//check if the current buffer is filled
+		if(this->bytesWritten >= this->bufferSize){
+			//emit the signal with the buffer's data
+			emit dataAvailable(static_cast<void*>(this->buffers[currentIndex].data()), this->params.bitDepth, this->params.samplesPerLine, this->params.linesPerFrame);
+
+			//prepare for the next buffer
 			this->currentIndex = (this->currentIndex + 1) % BUFFERS;
+			this->bytesWritten = 0; // Reset for the next buffer
 		}
-		this->bytesWritten = this->bytesWritten%this->bufferSize;
 	}
 }
 
-void DataReceiver::updateParams(ReceiverParameters params) {
-	this->params = params;
+void DataReceiver::updateParams(ReceiverParameters newParams) {
+	this->params = newParams;
 
-	size_t bytesPerSample = ceil(static_cast<double>(this->params.bitDepth) / 8.0);
-	size_t bufferSizeInBytes = this->params.samplesPerLine * this->params.linesPerFrame * this->params.framesPerBuffer * bytesPerSample;
+	int bytesPerSample = qCeil(static_cast<double>(this->params.bitDepth) / 8.0);
+	bufferSize = this->params.samplesPerLine * this->params.linesPerFrame * this->params.framesPerBuffer * bytesPerSample;
 
-	if(this->bufferSize != bufferSizeInBytes){
-		for(int i = 0; i < BUFFERS; i++){
-			this->buff[i] = static_cast<char*>(realloc(this->buff[i], bufferSizeInBytes)); //todo: check if realloc fails
-		}
-		this->bufferSize = bufferSizeInBytes;
+	for (auto &buffer : this->buffers) {
+		buffer.resize(bufferSize); //adjust buffer size based on new parameters
 	}
 }
 
@@ -89,10 +86,13 @@ void DataReceiver::updateParamsAndConnect(ReceiverParameters params) {
 }
 
 void DataReceiver::onConnect() {
-	this->socket->connectToHost(this->params.ip, this->params.port);
+	if(this->socket->state() == QTcpSocket::ConnectedState || this->socket->state() == QTcpSocket::ConnectingState){
+		this->socket->abort(); //ensure previous connections are closed before reconnecting
+	}
+	socket->connectToHost(params.ip, params.port);
 }
 
 void DataReceiver::onDisconnect() {
-	this->socket->disconnectFromHost();
-	this->bytesWritten = 0;
+	socket->disconnectFromHost();
+	bytesWritten = 0; //reset write position for new data
 }
--- SocketStreamClient/src/datareceiver.h
+++ SocketStreamClient/src/datareceiver.h
@@ -1,28 +1,28 @@
-/**
-**  This file is part of Socket Stream Client.
-**  Socket Stream Client can be used to test Socket Stream Extension for OCTproZ
-**  Copyright (C) 2020 Miroslav Zabic
-**
-**  Socket Stream Client is free software: you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, either version 3 of the License, or
-**  (at your option) any later version.
-**
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program. If not, see http://www.gnu.org/licenses/.
-**
-****
-** Author:	Miroslav Zabic
-** Contact:	zabic
-**			at
-**			iqo.uni-hannover.de
-****
-**/
+///**
+//**  This file is part of Socket Stream Client.
+//**  Socket Stream Client can be used to test Socket Stream Extension for OCTproZ
+//**  Copyright (C) 2020,2024 Miroslav Zabic
+//**
+//**  Socket Stream Client is free software: you can redistribute it and/or modify
+//**  it under the terms of the GNU General Public License as published by
+//**  the Free Software Foundation, either version 3 of the License, or
+//**  (at your option) any later version.
+//**
+//**  This program is distributed in the hope that it will be useful,
+//**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//**  GNU General Public License for more details.
+//**
+//**  You should have received a copy of the GNU General Public License
+//**  along with this program. If not, see http://www.gnu.org/licenses/.
+//**
+//****
+//** Author:	Miroslav Zabic
+//** Contact:	zabic
+//**			at
+//**			spectralcode.de
+//****
+//**/
 
 #ifndef DATARECEIVER_H
 #define DATARECEIVER_H
@@ -51,17 +51,14 @@ public:
 
 private:
 	QTcpSocket* socket;
-
 	ReceiverParameters params;
-
-	int currentIndex;
-	char* buff[BUFFERS];
-	size_t bufferSize;
-	size_t bytesWritten;
-
+	QVector<QByteArray> buffers;
+	int bufferSize = 0;
+	int currentIndex = 0;
+	int bytesWritten = 0;
 
 public slots:
-	void readIncommingData();
+	void readIncomingData();
 	void updateParams(ReceiverParameters params);
 	void updateParamsAndConnect(ReceiverParameters params);
 	void onConnect();
@@ -70,6 +67,10 @@ public slots:
 signals:
 	void dataAvailable(void* data, unsigned int bitDepth, unsigned int width, unsigned int height);
 	void connected(bool);
+
 };
 
 #endif // DATARECEIVER_H
+
+
+
